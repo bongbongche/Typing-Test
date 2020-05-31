a92ee7d1fee70e6ae8d1ede0ee29b91b71390f43
bongbong <60419514+bongbongche@users.noreply.github.com>
2020-05-31 17:26:23 +0900

    pug

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 import express from "express";
-import dotenv from "dotenv";
 import helmet from "helmet";
-
-dotenv.config();
+import routes from "./routes";
+import globalRouter from "./routers/globalRouter";
 
 const app = express();
 
 app.use(helmet());
+app.set("view engine", "pug");
+
+app.use(routes.home, globalRouter);
 
 export default app;
