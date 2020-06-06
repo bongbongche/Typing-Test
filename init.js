2242c4096c2338095c3fd61b7af581671a60ea41
bongbong <60419514+bongbongche@users.noreply.github.com>
2020-06-06 16:43:19 +0900

    heroku test

--- init.js
+++ init.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () => {
   console.log(`✅ Listening on: http://localhost:${PORT}`);
